35c236091f1fc2bec090f45aacbfac5659e286a2
Purbasa Maharana <purbasamaharana@gmail.com>
2026-02-22 16:50:20 +0530

    Modify filterChain method to throw Exception
    Added exception handling to filterChain method.

--- src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
+++ src/main/java/com/resume/ResumeMatcher/config/SecurityConfig.java
@@ -21,7 +21,7 @@ public class SecurityConfig {
     private JwtAuthFilter jwtAuthFilter;
 
     @Bean
-    public SecurityFilterChain filterChain(HttpSecurity http){
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception{
 
           http
                   .csrf(csrf -> csrf.disable())
